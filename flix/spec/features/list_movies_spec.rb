6eef299318bc22d911d7b000e9a41fd5f3f6f6d5
Matt Edwards <matt@chuckles>
2013-08-26 18:06:57 +1000

    Updated spec to include Movies

--- flix/spec/features/list_movies_spec.rb
+++ flix/spec/features/list_movies_spec.rb
@@ -2,10 +2,33 @@ require "spec_helper"
 
 describe "Viewing the list of movies" do
   it "shows the movies" do
+
+
+    movie1 = Movie.create(title: "Iron Man",
+                          rating: "PG-13",
+                          total_gross: 318412101.00,
+                          description: "Tony Stark builds an armored suit to fight the throes of evil",
+                          released_on: "2008-05-02")
+
+    movie2 = Movie.create(title: "Superman",
+                          rating: "PG",
+                          total_gross: 134218018.00,
+                          description: "Clark Kent grows up to be the greatest super-hero",
+                          released_on: "1978-12-15")
+
+    movie3 = Movie.create(title: "Spider-Man",
+                          rating: "PG-13",
+                          total_gross: 403706375.00,
+                          description: "Peter Parker gets bit by a genetically modified spider",
+                          released_on: "2002-05-03")
+
+    movies = [movie1, movie1, movie3]
     visit movies_url
 
-    expect(page).to have_text('3 Movies')
-    ['Movie 1', 'Movie 2', 'Movie 3'].each { |movie|  expect(page).to have_text(movie)}
+    expect(page).to have_text("#{movies.size} Movies")
+    movies.each do |movie|
+      expect(page).to have_text(movie.title)
+    end
   end
 
 end
