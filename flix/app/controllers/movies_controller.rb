b447ae8545fb7023f1b3796cb886ed76b0e4a331
Matt Edwards <matt@macbook>
2013-08-29 17:30:05 +1000

    Implemented the Create Action

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -21,4 +21,15 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def create
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie = Movie.new(movie_params)
+    @movie.save(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+
+
+  private
 end
