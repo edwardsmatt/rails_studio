b52cf2b9de99e710233aa291588e363167ef0abe
Matthew Edwards <matthew.edwards@dete.qld.gov.au>
2013-12-17 11:43:04 +1000

    Updated Form Helper in partial to redirect logically

--- flix/app/controllers/reviews_controller.rb
+++ flix/app/controllers/reviews_controller.rb
@@ -19,6 +19,10 @@ class ReviewsController < ApplicationController
     end
   end
 
+   def edit
+    @review = @movie.reviews.find(params[:id])
+  end
+
   def destroy
     @review = @movie.reviews.find(params[:id])
     @review.destroy
